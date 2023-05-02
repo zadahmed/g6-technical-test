83da4a0597c9d96d48ce3f0e5a00c5b67409c05a
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-05-02 09:40:49 +0100

    remove etf stuff

--- pulse_survey/settings.py
+++ pulse_survey/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "etf.evaluation.context_processors.space_name",
             ],
         },
     },
