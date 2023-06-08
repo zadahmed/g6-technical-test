1d1d69477c828d994a8979ed21ef4568965dcfad
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-06-08 14:09:35 +0100

    add allowed hosts

--- pulse_survey/settings.py
+++ pulse_survey/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+5+@2=z+)g!!_0eixnwcky7*^vyzt=im_hc+4k!9t+_qmt-e@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
