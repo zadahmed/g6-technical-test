b22b27f62d724da1566a99c8582e3e8bf0c38130
Zahid Ahmed <zahidzahmed@gmail.com>
2023-07-02 00:54:13 +0400

    test: update tests to ensure cabinet office emails are valid

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -2,18 +2,17 @@ from django.test import TestCase, SimpleTestCase
 
 from pulse_survey.survey import forms
 
-
-# TODO - these tests should be changed if we add validation for Cabinet Office emails
-
 class FeedbackFormTest(TestCase):
     def test_incorrect_email(self):
-        form = forms.FeedbackForm({"email": "not an email", "content": ""})
+        invalid_cabinet_email = "invalid@hmrc.com"
+        form = forms.FeedbackForm({"email": invalid_cabinet_email, "content": ""})
         form.is_valid()
         email_errors = form.errors["email"]
-        self.assertTrue("Enter a valid email address" in str(email_errors))
+        self.assertTrue("Only Cabinet Office email addresses are allowed." in str(email_errors))
 
     def test_correct_email(self):
-        form = forms.FeedbackForm({"email": "test@example.com", "content": "some feedback"})
+        valid_cabinet_email = "valid@cabinetoffice.gov.uk"
+        form = forms.FeedbackForm({"email": valid_cabinet_email, "content": "some feedback"})
         form.is_valid()
         no_email_errors = "email" not in form.errors
         self.assertTrue(no_email_errors)
@@ -21,7 +20,7 @@ class FeedbackFormTest(TestCase):
 
 class CabinetOfficeValidationTest(SimpleTestCase):
     def test_is_cabinet_office_email(self):
-        valid_email = "valid@example.com"
+        valid_email = "valid@cabinetoffice.gov.uk"
         result = forms.is_cabinet_office_email(valid_email)
         self.assertTrue(result)
 
